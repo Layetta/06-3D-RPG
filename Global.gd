0b255629df2409d1cccdf6a8ff185c703b68ce80
Layetta <aplawill@iu.edu>
2023-04-28 17:05:02 -0400

    Continued development

--- Global.gd
+++ Global.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	OS.window_position = Vector2(100,100)
+
 func _input(_event):
-	if Input.is_action_just_pressed("quit"):
+	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
